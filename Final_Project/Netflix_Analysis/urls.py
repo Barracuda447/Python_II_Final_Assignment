7b1e9c5f45f0a86f7414c3c58b97d1c6a0026fe0
Barracuda447 <144940016+Barracuda447@users.noreply.github.com>
2025-03-16 17:56:21 -0600

    Removed Accidental DJango app
    I had some github issues and accidentally copied over 2 Django apps.

--- Final_Project/Netflix_Analysis/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # home page / index
-    path('results/', views.results, name='results'),  # processed results page
-]
--- Final_Project/Netflix_Analysis/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'index.html')  # Render index.html
-
-def results(request):
-    return render(request, 'results.html')  # Render results.html
